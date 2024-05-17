0b4971b3e31483f645cd1ff4fd053d64ba0eaf4f
unreal-hw <ennisc21@gmail.com>
2024-05-17 16:55:38 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -41,6 +41,14 @@ async def on_message(message):
         embed.add_field(name=d, value=" ", inline=False)
         await message.channel.send(embed=embed, reference=message)
 
+    #동전
+    if message.content.startswith(f'{PREFIX}동전'):
+        c = random.choice('앞면','뒷면')
+        embed=discord.Embed(description=":coin: 팅그르르...",
+                            color=0x61b866)
+        embed.add_field(name=c, value=" ", inline=False)
+        await message.channel.send(embed=embed, reference=message)
+
 
 try:
     client.run(TOKEN)
